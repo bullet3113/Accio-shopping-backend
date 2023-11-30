7aca28604d4dfb03fd18855a7e85a27aec1e0f3e
bullet3113 <0hitesh.a@gmail.com>
2023-11-30 23:41:28 +0530

    initial commit

---
+++ src/main/java/com/accioshoppingbackend/Accio/Shopping/Backend/AccioShoppingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.accioshoppingbackend.Accio.Shopping.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccioShoppingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccioShoppingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accioshoppingbackend/Accio/Shopping/Backend/AccioShoppingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accioshoppingbackend.Accio.Shopping.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccioShoppingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
